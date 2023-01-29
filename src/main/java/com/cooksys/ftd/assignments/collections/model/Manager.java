c480adc2610622b2c7b7a315d3da56e155e31604
Nataly Villalba <alexandra191192@hotmail.com>
2023-01-29 17:10:50 -0500

    Implemented Worker and Manager classes

--- src/main/java/com/cooksys/ftd/assignments/collections/model/Manager.java
+++ src/main/java/com/cooksys/ftd/assignments/collections/model/Manager.java
@@ -1,89 +1,73 @@
 package com.cooksys.ftd.assignments.collections.model;
 
-import com.cooksys.ftd.assignments.collections.util.MissingImplementationException;
-
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
-/**
- * TODO: Implement this class
- *  <br><br>
- *  A manager is a type of employee that can be a superior to another employee.
- *  <br>
- *  A manager should have a name, and, optionally, a manager superior to them.
- */
 public class Manager implements Employee {
 
-    // TODO: Does this class need private fields? If so, add them here
+    private final String name;
+    private Manager manager;
 
     /**
-     * TODO: Implement this constructor.
-     *
      * @param name the name of the manager to be created
      */
     public Manager(String name) {
-        throw new MissingImplementationException();
+        this.name = name;
     }
 
     /**
-     *  TODO: Implement this constructor.
-     *
-     * @param name the name of the manager to be created
+     * @param name    the name of the manager to be created
      * @param manager the direct manager of the manager to be created
      */
     public Manager(String name, Manager manager) {
-        throw new MissingImplementationException();
+        this.name = name;
+        this.manager = manager;
     }
 
     /**
-     * TODO: Implement this getter.
-     *
      * @return the name of the manager
      */
     @Override
     public String getName() {
-        throw new MissingImplementationException();
+        return this.name;
     }
 
     /**
-     * TODO: Implement this getter.
-     *
      * @return {@code true} if this manager has a manager, or {@code false} otherwise
      */
     @Override
     public boolean hasManager() {
-        throw new MissingImplementationException();
+        return this.manager != null;
     }
 
     /**
-     * TODO: Implement this getter.
-     *
      * @return the manager of this manager, or {@code null} if it has none
      */
     @Override
     public Manager getManager() {
-        throw new MissingImplementationException();
+        return this.manager;
     }
 
     /**
-     * TODO: Implement this method.
-     *  <br><br>
-     *  Retrieves the manager's chain of command as a {@code List<Manager>}, starting with their direct {@code Manager},
-     *  followed by that {@code Manager}'s {@code Manager}, and so on, until the top of the hierarchy is reached.
-     *  <br><br>
-     *  The returned list should never be or contain {@code null}.
-     *  <br><br>
-     *  If the manager does not have a {@code Manager}, an empty
-     *  {@code List<Manager>} should be returned.
-     *
      * @return a {@code List<Manager>} that represents the manager's chain of command,
      */
     @Override
     public List<Manager> getChainOfCommand() {
-        throw new MissingImplementationException();
+        List<Manager> chainOfCommand = new LinkedList<>();
+        Employee currentEmployee = this;
+        while (currentEmployee.hasManager()) {
+            chainOfCommand.add(currentEmployee.getManager());
+            currentEmployee = currentEmployee.getManager();
+        }
+        return chainOfCommand;
     }
 
-    // TODO: Does this class need custom .equals() and .hashcode() methods? If so, implement them here.
-
-    // TODO [OPTIONAL]: Consider adding a custom .toString() method here if you want to debug your code with System.out.println() statements
-
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Manager manager1 = (Manager) o;
+        return name.equals(manager1.name) && Objects.equals(manager, manager1.manager);
+    }
 }
